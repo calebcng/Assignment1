7afc02aee5d4e1c0afc1866216b8ac072b68d698
calebcng <calebcng@gmail.com>
2013-10-28 18:41:21 -0700

    Updated IO.cpp
    Additional detailed pseudo code for P1-C1 and P3-C3, as well as input
    code for exiting simulation.

--- ElevatorSystem/IO.cpp
+++ ElevatorSystem/IO.cpp
@@ -8,9 +8,6 @@ I/O Program
 Link to datapools, semaphores, open pipeline
 install signal handler using intercept() primitive
 */
-
-//Testing
-
 struct LiftDataPool
 {
 	int CurrentFloor;
@@ -41,60 +38,80 @@ int main()
 
 	//Initialize variables
 	char input[2];
+	int Lift1Floor;
+	int Lift1Direction;
+	int Lift1Status;
+	int Lift1DoorStatus;
 
-do {
-	//test keyboard for 2 characters
-	input[0] = getch();
-	input[1] = getch();
+	do {
+		//test keyboard for 2 characters
+		input[0] = getch();
+		input[1] = getch();
 
-//	if(2 characters present) {
-//		read them,
-//		check they make sense and if so write them to the pipeline
-//	}
+	//	if(2 characters present) {
+	//		read them,
+	//		check they make sense and if so write them to the pipeline
+	//	}
 
-	//Check inputs to make sure they are valid
-	if( input[0]=='U' || input[0]=='D' || input[0]=='1' || input[0]=='2' ) {
-		if( input[1]=='0' || input[1]=='1' || input[1]=='2' || input[1]=='3' || input[1]=='4' || input[1]=='5' || input[1]=='6' ||
-			input[1]=='7' || input[1]=='8' || input[1]=='9' )
-		{
-			//Input characters make sense. Proceed to send to Dispatcher.
-			printf( "Input characters make sense. Proceeding to Dispatcher.\n" );
+		//Check inputs to make sure they are valid
+		if( input[0]=='U' || input[0]=='D' || input[0]=='1' || input[0]=='2' ) {
+			if( input[1]=='0' || input[1]=='1' || input[1]=='2' || input[1]=='3' || input[1]=='4' || input[1]=='5' || input[1]=='6' ||
+				input[1]=='7' || input[1]=='8' || input[1]=='9' )
+			{
+				//Input characters make sense. Proceed to send to Dispatcher.
+				printf( "Input characters make sense. Proceeding to Dispatcher.\n" );
+			}
+			else {
+				printf( "Invalid inputs.\n" );
+				continue;
+			}
+		}
+		else if( input[0]=='e' && input[1] == 'e' ) {
+			//trigger end simulation flag
 		}
 		else {
 			printf( "Invalid inputs.\n" );
 			continue;
 		}
-	}
-	else {
-		printf( "Invalid inputs.\n" );
-		continue;
-	}
 
-/*	
-	if(read_semaphore(PS3) > 0) car1 produced data 
-		WAIT(PS3) ;
-		read car1’s datapool into local structure
-		SIGNAL(CS3) ;
-		Redraw Lift1 screen
-	} 
-*/
-	if( P1 > 0 ) {
-		P1.Wait(); 
-		int Lift1Floor = Lift1;
-		int Lift1Direction;
-		int Lift1Status;
-		int Lift1DoorStatus;
-	}
-		
-
-	if(read_semaphore(PS4) > 0) /* car2 produced data */
-		WAIT(PS4) ;
-		read car2’s datapool into local structure
-		SIGNAL(CS4) ;
-		Redraw Car2 screen
-	}
-} while(end of simulation flag is not TRUE) /* do until end of simulation*/
-end program
+	/*	
+		if(read_semaphore(PS3) > 0) car1 produced data 
+			WAIT(PS3) ;
+			read car1’s datapool into local structure
+			SIGNAL(CS3) ;
+			Redraw Lift1 screen
+		} 
+	*/
+		if( P1.Read() > 0 ) {
+			//WAIT(PS1)
+			P1.Wait();
+			//read Lift1's datapool into local structure
+			Lift1Floor = Lift1->CurrentFloor;
+			Lift1Direction = Lift1->Direction;
+			Lift1Status = Lift1->LiftStatus;
+			Lift1DoorStatus = Lift1->DoorStatus;
+			//Signal(CS1)
+			C1.Signal();
+			//Redraw Lift1 screen
+		}
+			
+		if( P3.Read() > 0 ) {
+			//WAIT(PS3)
+			P3.Wait();
+			//read Lift2's datapool into local structure
+			Lift2Floor = Lift2->CurrentFloor;
+			Lift2Direction = Lift2->Direction;
+			Lift2Status = Lift2->LiftStatus;
+			Lift2DoorStatus = Lift2->DoorStatus;
+			//Signal(CS1)
+			C3.Signal();
+			//Redraw Lift2 screen
+		}
+	} while(end of simulation flag is not TRUE) /* do until end of simulation*/
+	
+	//end program
+	return 0;
+}
 
 
 Signal_handler() /* installed with intercept primitive, dispatcher sends this process signals */
